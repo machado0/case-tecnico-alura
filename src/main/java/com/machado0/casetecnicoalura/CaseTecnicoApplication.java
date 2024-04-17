e5cc2c010cc21197466531c2d4756a903bb7abdf
machado0 <medmachado27@gmail.com>
2024-04-17 12:15:47 -0300

    initial commit

---
+++ src/main/java/com/machado0/casetecnicoalura/CaseTecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.machado0.casetecnicoalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseTecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseTecnicoApplication.class, args);
+	}
+
+}
